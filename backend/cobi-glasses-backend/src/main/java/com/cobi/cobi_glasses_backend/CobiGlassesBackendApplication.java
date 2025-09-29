3d187148bcc591a7a79c542618735db0ae291486
Obi <josephclintonchukwudi.obi@chevron.com>
2025-09-29 11:24:56 +0100

    added mobile application directory with expo and a springboot backend directory

---
+++ backend/cobi-glasses-backend/src/main/java/com/cobi/cobi_glasses_backend/CobiGlassesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cobi.cobi_glasses_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobiGlassesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobiGlassesBackendApplication.class, args);
+	}
+
+}
---
+++ backend/cobi-glasses-backend/src/test/java/com/cobi/cobi_glasses_backend/CobiGlassesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobi.cobi_glasses_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobiGlassesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
