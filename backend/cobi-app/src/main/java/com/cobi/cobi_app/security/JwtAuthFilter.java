5c20e3a7dce7016bd969bb627975693e8012214d
Obi <josephclintonchukwudi.obi@chevron.com>
2025-10-20 18:03:07 +0100

    changes to backend

--- backend/cobi-app/src/main/java/com/cobi/cobi_app/security/JwtAuthFilter.java
+++ backend/cobi-app/src/main/java/com/cobi/cobi_app/security/JwtAuthFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 
@@ -20,9 +21,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                @NonNull HttpServletResponse response,
+                                @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
